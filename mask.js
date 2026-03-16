dae249019949b20f0e11ce9cd948d6ba82ce74d0
valilaisss <vasilissa.ne@gmail.com>
2026-03-16 17:29:26 +0300

    яысч

--- mask.js
+++ mask.js
@@ -1,36 +1,37 @@
 let img;
 let secondImg;
 let revealed = false;
-let maskLayer;
-function setup() {
 
+function preload() {
+    img = loadImage('assets/mask.png')
+    secondImg = loadImage('assets/_ (37).jpeg')
+}
+
+function setup() {
     windowWidth = window.innerWidth;
     windowHeight = window.innerHeight;
     
     myCanvas = createCanvas(windowWidth, windowHeight)
     myCanvas.parent("#canvas-parent")
-
     maskLayer = createGraphics(windowWidth, windowHeight)
-
 }
 
 function draw() {
-
     maskLayer.clear();
     maskLayer.fill(255);
-    
+
     maskLayer.circle(
         width * 0.3531,
         height * 0.5083,
         width * 0.2948
     );
-    
+
     maskLayer.circle(
         width * 0.6055,
         height * 0.2912,
-        width * 0.2026
+        width * 0.2026 
     );
-    
+
     maskLayer.circle(
         width * 0.9797,
         height * 0.8176,
@@ -42,34 +43,30 @@ function draw() {
     
     let maskedImg = img.get(); 
     maskedImg.mask(maskLayer);
-
+    
     image(maskedImg, 0, 0, width, height);
 
-    if (frameCount % 20 === 0) {
-        checkCanvasFill()
-    }
-
+    checkCanvasFill()
     if (revealed) {
         image(secondImg, 0, 0, width, height)
     }
-
 }
 
 window.addEventListener('resize', () => {
-
     resizeCanvas(window.innerWidth, window.innerHeight);
     maskLayer = createGraphics(width, height);
-
     redraw();
-
 });
 
 function checkCanvasFill() {
 
     if (revealed) return
 
-    maskLayer.loadPixels()
-    const data = maskLayer.pixels
+    const canvas = document.querySelector('canvas')
+    const ctx = canvas.getContext('2d')
+
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height)
+    const data = imageData.data
 
     let painted = 0
     let total = data.length / 4
