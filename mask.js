1103f610610ab76cea3a8cd1b8aed5ceb5314bf4
valilaisss <vasilissa.ne@gmail.com>
2026-03-26 05:38:53 +0300

    выаячс

--- mask.js
+++ mask.js
@@ -102,12 +102,10 @@ const maskCircles = [
             leftImg.addEventListener('click', () => {
                 leftImg.classList.add('slide-out');
                 rightImg.classList.add('slide-out');
-                setTimeout(() => overlay.remove(), 1200);
             });
             rightImg.addEventListener('click', () => {
                 leftImg.classList.add('slide-out');
                 rightImg.classList.add('slide-out');
-                setTimeout(() => overlay.remove(), 1200);
             });
         }
                 }
